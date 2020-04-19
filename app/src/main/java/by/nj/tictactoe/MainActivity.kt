b44ba8141be52a50f3f7462979063dc50b40d27c
XiveZ <jumrsup@hi2.in>
2020-04-19 18:47:59 +0300

    feat: add 'restart' function

--- app/src/main/java/by/nj/tictactoe/MainActivity.kt
+++ app/src/main/java/by/nj/tictactoe/MainActivity.kt
@@ -4,9 +4,17 @@ import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private var Player: Int = 1;
+    private var player1 = ArrayList<Int>();
+    private var player2 = ArrayList<Int>();
+    private  var player1WinsCounts = 0;
+    private  var player2WinsCounts = 0;
 
     fun onClick(view: View) {
         var a: Int = 0;
@@ -25,9 +33,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         gamePlay(a, btnID)
     }
 
-    private var Player: Int = 1;
-    private var player1 = ArrayList<Int>();
-    private var player2 = ArrayList<Int>();
+
 
     private fun gamePlay(numberPlayer: Int, btnID: Button) {
         if (this.Player == 1) {
@@ -36,6 +42,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             btnID.setBackgroundColor(resources.getColor(android.R.color.holo_blue_light));
             player1.add(numberPlayer);
             Player = 2;
+            autoplay();
         } else {
             btnID.text = "O";
             btnID.setTextColor(resources.getColor(android.R.color.white))
@@ -43,8 +50,37 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             player2.add(numberPlayer)
             Player = 1;
         }
-        btnID.isEnabled = false;
         winner();
+    }
+
+    private fun autoplay() {
+        var emptyCells = ArrayList<Int>();
+
+        for (cellID in 1..9){
+            if (!(player1.contains(cellID) || player2.contains(cellID))){
+                emptyCells.add(cellID);
+            }
+        }
+
+        if (emptyCells.size == 0)restartGame();
+
+        var random: Int = emptyCells[Random.nextInt(emptyCells.size)];
+        var buSelected:Button?;
+        buSelected = when(random){
+            1 -> bt1;
+            2 -> bt2;
+            3 -> bt3;
+            4 -> bt4;
+            5 -> bt5;
+            6 -> bt6;
+            7 -> bt7;
+            8 -> bt8;
+            9 -> bt9;
+            else -> bt1;
+        }
+
+        gamePlay(random, buSelected);
+
     }
 
     private fun winner() {
@@ -55,8 +91,36 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         if (player2.containsAll(arrayListOf(1, 2, 3)) || player2.containsAll(arrayListOf(4, 5, 6)) || player2.containsAll(arrayListOf(7, 8, 9)) ||
             player2.containsAll(arrayListOf(1, 4, 7)) || player2.containsAll(arrayListOf(2, 5, 8)) || player2.containsAll(arrayListOf(3, 6, 9))) winner = 2
 
-        if (winner == 1) Toast.makeText(this, "Winner player 1", Toast.LENGTH_LONG).show()
-        if (winner == 2) Toast.makeText(this, "Winner player 2", Toast.LENGTH_LONG).show()
+        if (winner == 1){player1WinsCounts+=1; Toast.makeText(this, "Winner player 1", Toast.LENGTH_LONG).show(); restartGame()}
+        if (winner == 2) {player2WinsCounts+=1; Toast.makeText(this, "Winner player 2", Toast.LENGTH_LONG).show(); restartGame()}
 
+    }
+
+    fun restartGame(){
+        Player = 1;
+        player1.clear();
+        player2.clear()
+
+        for (index in 1..9){
+            var buSelected:Button?;
+            buSelected = when(index){
+                1 -> bt1;
+                2 -> bt2;
+                3 -> bt3;
+                4 -> bt4;
+                5 -> bt5;
+                6 -> bt6;
+                7 -> bt7;
+                8 -> bt8;
+                9 -> bt9;
+                else -> bt1;
+            }
+            buSelected.text = ""
+            buSelected.setBackgroundResource(android.R.color.white);
+            buSelected.isEnabled = true;
+
+        }
+
+        Toast.makeText(this, "Player 1: wins - $player1WinsCounts,\n Player2: wins - $player1WinsCounts", Toast.LENGTH_LONG).show()
     }
 }
